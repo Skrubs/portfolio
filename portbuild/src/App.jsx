bd6c00d22afb551cb3ab94ad398913a1b43135ed
Angelo <barcela2013@gmail.com>
2024-06-19 09:54:13 -0500

    Setting Stack to PERN

--- portbuild/src/App.jsx
+++ portbuild/src/App.jsx
@@ -1,41 +1,14 @@
 
 import './App.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import PrimarySearchAppBar from "./components/Appbar.jsx";
-import BasicGrid from "./components/GridContainer.jsx"
-import Footer from "./components/Footer.jsx";
-import Navigationbar from "./components/Navigationbar.jsx";
+
+
 
 function App() {
 
 
   return (
-    <>
-        <div style={{
-            backgroundColor: "black",
-            padding: "10px",
-            marginLeft: "2em",
-            marginRight: "2em",
-            borderRadius: "20px",
-
-        }}><PrimarySearchAppBar/></div>
-        <div style={{
-            backgroundColor: "gray",
-            borderRadius: "5px",
-            border: "solid",
-            borderWidth: "2px",
-            width: " 20% ",
-        }}>
-            <Navigationbar/>
-
-        </div>
-        <BasicGrid name={'Angelo'}/>
-        <Footer/>
-
-    </>
+   <>
+   </>
   )
 }
 
