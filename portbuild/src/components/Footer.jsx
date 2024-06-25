1fd277c6f6f919b08269b9950300dd99aba64fbc
William Stansbury <williamrstansbury@gmail.com>
2024-06-25 18:53:04 -0400

    Fix footer to update time automatically

--- portbuild/src/components/Footer.jsx
+++ portbuild/src/components/Footer.jsx
@@ -1,29 +1,31 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-export default function Footer(){
-    const [currentDate, setCurrentDate] = useState(Date);
+export default function Footer() {
+    const [currentDate, setCurrentDate] = useState(new Date());
 
     useEffect(() => {
-        setCurrentDate(Date);
+        const interval = setInterval(() => {
+            setCurrentDate(new Date());
+        }, 1000);
 
+        return () => clearInterval(interval); // Cleanup interval on component unmount
     }, []);
 
-    return(
+    return (
         <>
-            <div style={{   marginTop: `80vh`,
-                            border: 'solid gold 2px',
-                            backgroundColor: 'darkslateblue',
-                            flex: 'auto',
-                            width: '40%',
-                            marginLeft: '30%',
-                            height: '14vh',
-                            display: 'block',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-
-                        }}>
-
-                    <p>This is the footer. {currentDate}</p>
+            <div style={{
+                marginTop: '80vh',
+                border: 'solid gold 2px',
+                backgroundColor: 'darkslateblue',
+                flex: 'auto',
+                width: '40%',
+                marginLeft: '30%',
+                height: '14vh',
+                display: 'block',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <p>This is the footer. {currentDate.toLocaleString()}</p>
             </div>
         </>
     );
