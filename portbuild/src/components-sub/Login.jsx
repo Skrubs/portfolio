08c0a0d2f5854824d117c245ce6da120bac1e205
alanv <alanvang91@gmail.com>
2024-07-11 12:12:17 -0500

    change login.jsx to have input instead of textarea so it does not allow multiline inputs. Also added type="password" to password input to hide password while being inputted.

--- portbuild/src/components-sub/Login.jsx
+++ portbuild/src/components-sub/Login.jsx
@@ -10,8 +10,8 @@ export default function Login({ x, y }) {
 
     return (
         <div className="styled-div" style={divStyle}>
-            <textarea className="styled-textarea" placeholder="Name"/>
-            <textarea className="styled-textarea" placeholder="Password" />
+            <input className="styled-textarea" placeholder="Name"/>
+            <input className="styled-textarea" type="password" placeholder="Password" />
             <div className="button-container">
                 <button className="styled-button">Submit</button>
                 <button className="styled-button">Cancel</button>
