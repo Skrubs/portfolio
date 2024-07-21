4eda6043e5e6e6123911e5e0ccbd2f931fc50cd8
Angelo <barcela2013@gmail.com>
2024-07-21 17:00:40 -0500

    fixed bug with body in app.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import {BrowserRouter} from "react-router-dom";
 import AppRoutes from "./AppRoutes.jsx";
+import Home from './components/Home.jsx';
 
 
 
@@ -10,6 +11,7 @@ function App() {
   return (
       <div>
           <BrowserRouter>
+              <Home/>
             <AppRoutes/>
           </BrowserRouter>
       </div>
